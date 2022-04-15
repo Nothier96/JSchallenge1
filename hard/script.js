3510d9b201daa4d1352e3e05919850883eb1daae
Thierno Sissoko <thiernosissoko@AdministorsMBP2.attlocal.net>
2022-04-14 22:31:46 -0400

    pushing hard

---
+++ hard/script.js
@@ -0,0 +1,16 @@
+function add(number1 , number2){
+    return console.log(`${number1} + ${number2} = ${number1 + number2}`) ; 
+}
+function multiply(number1 , number2){
+    return console.log(`${number1} * ${number2} = ${number1 * number2}`) ; 
+}
+function substract(number1 , number2){
+    return console.log(`${number1} - ${number2} = ${number1 - number2}`) ; 
+}
+function divide(number1 , number2){
+    return console.log(`${number1} / ${number2} = ${number1 / number2}`) ; 
+}
+add(12,34);
+multiply(23,4);
+divide(50,10);
+substract(45,23); 
