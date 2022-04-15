7a28a5b7ac96194102b10151ffeb024d4cb8d191
Thierno Sissoko <thiernosissoko@AdministorsMBP2.attlocal.net>
2022-04-14 21:56:00 -0400

    pushing easy

---
+++ easy/script.js
@@ -0,0 +1,7 @@
+let firstName = 'toureba' ; 
+let lastName = "nothiersissoko" ; 
+if (firstName.length > lastName.length) {
+    console.log(`The name ${firstName} is longer than ${lastName} by ${firstName.length - lastName.length} characters`);
+} else {
+    console.log(`The name ${lastName} is longer than ${firstName} by ${lastName.length - firstName.length} characters`);
+}
