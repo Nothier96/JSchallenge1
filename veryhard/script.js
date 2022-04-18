16a8941a861071aef56c82acd934558d675d2f66
Thierno Sissoko <thiernosissoko@AdministorsMBP2.attlocal.net>
2022-04-18 11:23:29 -0400

    some changes in the very hard

--- veryhard/script.js
+++ veryhard/script.js
@@ -14,19 +14,18 @@ function divide(number1 , number2){
 const number1 = Number(prompt("Please enter the first number"));
 const operation = prompt("Enter the operation"); 
 const number2 = Number(prompt("Please enter the second number"));
+
+
 if (operation === '+' || operation === 'addition') {
     add(number1 , number2);
-}
-if (operation === '-' || operation === 'substract') {
+} else if (operation === '-' || operation === 'substract') {
     substract(number1 , number2);
-}
-
-if (operation === '/' || operation === 'divide') {
+} else if (operation === '/' || operation === 'divide') {
     divide(number1 , number2);
-}
-
-if (operation === '*' || operation === 'multiply') {
+} else if (operation === '*' || operation === 'multiply') {
     multiply(number1 , number2);
+} else {
+    alert("Not an operation"); 
 }
 
 
